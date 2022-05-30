2ef7b01791f738aa22c5145d89e2062f8e4ab5d0
Chris Athanas <realityexpander@gmail.com>
2022-05-30 02:41:56 -0500

    Add DI

---
+++ app/src/main/java/com/realityexpander/stockmarketapp/StockApplication.kt
@@ -0,0 +1,8 @@
+package com.realityexpander.stockmarketapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class StockApplication: Application() {
+}
---
+++ app/src/main/java/com/realityexpander/stockmarketapp/di/RepositoryModule.kt
@@ -0,0 +1,29 @@
+package com.realityexpander.stockmarketapp.di
+
+import com.realityexpander.stockmarketapp.data.csv.CSVParser
+import com.realityexpander.stockmarketapp.data.csv.CompanyListingsParser
+import com.realityexpander.stockmarketapp.data.repository.StockRepositoryImpl
+import com.realityexpander.stockmarketapp.domain.model.CompanyListing
+import com.realityexpander.stockmarketapp.domain.repository.StockRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindCompanyListingsParser(
+        companyListingsParser: CompanyListingsParser
+    ): CSVParser<CompanyListing>
+
+    @Binds
+    @Singleton
+    abstract fun bindStockRepository(
+        stockRepositoryImpl: StockRepositoryImpl
+    ): StockRepository
+}
