d9d3fa045276bc02f2abdef4f0ce34b9d21a4e7e
Chris Athanas <realityexpander@gmail.com>
2022-10-21 20:39:34 -0500

    Fix error message during test

--- app/src/main/java/com/realityexpander/stockmarketapp/presentation/MainViewModel.kt
+++ app/src/main/java/com/realityexpander/stockmarketapp/presentation/MainViewModel.kt
@@ -3,14 +3,28 @@ package com.realityexpander.stockmarketapp.presentation
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.realityexpander.stockmarketapp.data.RepositoryModel
+import com.realityexpander.stockmarketapp.data.csv.CSVParser
+import com.realityexpander.stockmarketapp.data.remote.dto.CompanyInfoDTO
+import com.realityexpander.stockmarketapp.data.remote.dto.StockApi
 import com.realityexpander.stockmarketapp.domain.GetRepositoriesUseCase
+import com.realityexpander.stockmarketapp.domain.model.IntradayInfo
+import com.realityexpander.stockmarketapp.util.Resource
 import com.realityexpander.stockmarketapp.utils.LiveDataResult
 import kotlinx.coroutines.*
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.ResponseBody
+import okhttp3.ResponseBody.Companion.toResponseBody
+import okio.IOException
+import retrofit2.HttpException
+import java.io.InputStream
+import java.io.Reader
+import java.time.LocalDateTime
 
 class MainViewModel(
     mainDispatcher : CoroutineDispatcher,
     ioDispatcher : CoroutineDispatcher,
-    private val getRepositoriesUseCase: GetRepositoriesUseCase
+    private val getRepositoriesUseCase: GetRepositoriesUseCase,
+    val api: StockApi = StockApiFake()
     ) : ViewModel() {
 
         private val job = SupervisorJob()
@@ -43,4 +57,59 @@ class MainViewModel(
             super.onCleared()
             this.job.cancel()
         }
+
+        suspend fun getIntradayInfos(stockSymbol: String): Resource<List<IntradayInfo>> {
+            val parser = IntradayInfoCSVParserFake()
+
+            return try {
+                val response = api.getIntradayInfoRawCSV(stockSymbol)
+                // println(response.readBytes().toString(Charsets.UTF_8)) // keep for debugging
+
+                val results = parser.parse(response.byteStream())
+                Resource.Success(results)
+            } catch (e: IOException) { // parse error
+                //e.printStackTrace()
+                Resource.Error(e.localizedMessage ?: "Error loading or parsing intraday info")
+            } catch (e: HttpException) { // invalid network response
+                //e.printStackTrace()
+                Resource.Error(e.localizedMessage ?: "Error with network for intraday info")
+            } catch (e: Exception) { // unknown error
+                //e.printStackTrace()
+                Resource.Error(e.localizedMessage ?: "Unknown Error loading or parsing intraday info")
+            }
+        }
+}
+
+class StockApiFake: StockApi {
+    override suspend fun getListOfStocksRawCSV(apiKey: String): ResponseBody {
+        return ("timestamp,open,high,low,close,volume" + System.lineSeparator() +
+                "2021-01-01,1.0,2.0,3.0,4.0,5.0" + System.lineSeparator() +
+                "2021-01-02,1.0,2.0,3.0,4.0,5.0" + System.lineSeparator()).toResponseBody("text/csv".toMediaTypeOrNull())
+    }
+
+    override suspend fun getIntradayInfoRawCSV(symbol: String, apiKey: String): ResponseBody {
+        return ("timestamp,open,high,low,close,volume" + System.lineSeparator() +
+                "2021-01-01,1.0,2.0,3.0,4.0,5.0" + System.lineSeparator() +
+                "2021-01-02,1.0,2.0,3.0,4.0,5.0" + System.lineSeparator()).toResponseBody("text/csv".toMediaTypeOrNull())
+    }
+
+    override suspend fun getCompanyInfo(symbol: String, apiKey: String): CompanyInfoDTO {
+        return CompanyInfoDTO(
+            symbol = "TEST",
+            companyName = "Test Company",
+            exchange = "Test Exchange",
+            industry = "Test Industry",
+            description = "Test Description",
+            country = "Test Country",
+        )
+    }
+}
+
+class IntradayInfoCSVParserFake: CSVParser<IntradayInfo> {
+    override suspend fun parse(csvStream: InputStream): List<IntradayInfo> {
+        return listOf(
+            IntradayInfo(LocalDateTime.parse("2021-01-01T00:00:00"), 1.0, 2.0, 3.0, 4.0, 5000),
+            IntradayInfo(LocalDateTime.parse("2021-02-01T01:00:30"), 2.0, 3.0, 4.0, 5.0, 6000),
+        )
+    }
 }
--- app/src/test/java/com/realityexpander/stockmarketapp/presentation/MainViewModelTest.kt
+++ app/src/test/java/com/realityexpander/stockmarketapp/presentation/MainViewModelTest.kt
@@ -2,13 +2,17 @@ package com.realityexpander.stockmarketapp.presentation
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.realityexpander.stockmarketapp.data.RepositoryModel
+import com.realityexpander.stockmarketapp.data.remote.dto.StockApi
 import com.realityexpander.stockmarketapp.domain.GetRepositoriesUseCase
+import com.realityexpander.stockmarketapp.util.Resource
 import com.realityexpander.stockmarketapp.utils.LiveDataResult
 import io.mockk.MockKAnnotations
 import io.mockk.coEvery
 import io.mockk.impl.annotations.MockK
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -24,13 +28,16 @@ class MainViewModelTest {
     @MockK
     lateinit var getRepositoriesUseCase: GetRepositoriesUseCase
 
+    @MockK
+    lateinit var api: StockApi
+
     lateinit var mainViewModel: MainViewModel
     private val dispatcher = Dispatchers.Unconfined
 
     @Before
     fun setup() {
         MockKAnnotations.init(this)
-        mainViewModel = MainViewModel(dispatcher, dispatcher, getRepositoriesUseCase)
+        mainViewModel = MainViewModel(dispatcher, dispatcher, getRepositoriesUseCase, api)
     }
 
     @Test
@@ -59,6 +66,23 @@ class MainViewModelTest {
 
         assert(mainViewModel.repositoriesLiveData.value != null)
         assert(mainViewModel.repositoriesLiveData.value!!.status == LiveDataResult.STATUS.ERROR)
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun testFetchIntradayInfosNegative() {
+//        coEvery { mainViewModel.getIntradayInfos(any()) } coAnswers {
+        coEvery { api.getIntradayInfoRawCSV(any()) } coAnswers {
+            throw Exception("No network")
+        }
+
+        runTest {
+            val response = mainViewModel.getIntradayInfos("irtizakh")
+
+            assert(response is Resource.Error)
+            assert(response.message != null)
+            assert(response.message == "No network")
+        }
     }
 
 }
